36c01dda4aa3378700d7afe844ed980c4f29822d
Priyanshu creator <vermapriyanshu422@gmail.com>
2022-09-17 14:53:56 +0530

    ...

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,20 +1,23 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
-import { allPostsQuery } from '../../../utils/queries';
-import { client } from '../../../utils/client';
+import { allPostsQuery } from "../../../utils/queries";
+import { client } from "../../../utils/client";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
     const query = allPostsQuery();
 
     const data = await client.fetch(query);
 
     res.status(200).json(data);
-  } else if (req.method === 'POST') {
+  } else if (req.method === "POST") {
     const doc = req.body;
 
     client.create(doc).then(() => {
-      res.status(200).json('video created');
+      res.status(200).json("video created");
     });
   }
 }
